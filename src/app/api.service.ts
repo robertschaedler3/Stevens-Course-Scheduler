f94ac96db252db302e7cec6423374465474450d2
Robert Schaedler III <robertschaedler3@gmail.com>
2019-10-27 15:57:20 -0400

    refactor get requests

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,9 +1,50 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of, BehaviorSubject } from 'rxjs';
+import { tap, map } from 'rxjs/operators';
+import * as _ from 'lodash';
+
+import { RawSection } from './scheduler.model';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  private baseUrl = 'https://stevens-scheduler.cfapps.io/p';
+  terms$: Observable<string[]>;
+  courses$: Observable<Array<RawSection>>;
+
+  // private selectedSectionSource: BehaviorSubject<string> = new BehaviorSubject('');
+  // currentSelectedSection = this.selectedSectionSource.asObservable();
+
+  constructor(private http: HttpClient) {
+    this.getTerms();
+    this.preloadCourses();
+  }
+
+  private preloadCourses() {
+    this.terms$.subscribe(terms => {
+      this.courses$ = this.getCourses(terms[0])
+    });
+  }
+
+  getTerms() {
+    return this.terms$ = this.http.get<string[]>(this.baseUrl + '/terms').pipe(
+      map(data => _.values(data)),
+      // tap(console.log),
+    );
+  }
+
+  getCourses(term: string) {
+    return this.courses$ = this.http.get<Array<RawSection>>(this.baseUrl + '/' + term).pipe(
+      map(data => _.values(data)),
+      tap(console.log),
+    );
+  }
+
+
+
+
 }
