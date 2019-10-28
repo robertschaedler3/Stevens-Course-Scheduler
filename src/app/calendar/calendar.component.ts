ec69dbc272531bb8278c7c8a22ba775f3c0d2f75
Robert Schaedler III <robertschaedler3@gmail.com>
2019-10-28 16:26:26 -0400

    refactored week day columns to separate components

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ViewChildren, QueryList } from '@angular/core';
 
 
 export interface Tile {
@@ -13,52 +13,33 @@ export interface Tile {
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.scss']
 })
-export class CalendarComponent implements OnInit {
+export class CalendarComponent implements AfterViewInit {
+
+  @ViewChildren('weekdays') weekdayCols: QueryList<any>;
 
   hours: String[] = ["8AM", "9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM", "6PM", "7PM", "8PM"];
   days: String[] = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
 
-  // mainGrid: Tile[] = [];
-  mainGrid: number[] = [];
-
-  tiles: Tile[] = [
-    { text: '', cols: 1, rows: 1, color: '#fff' },
-    { text: 'Monday', cols: 2, rows: 1, color: '#fff' },
-    { text: 'Tuesday', cols: 2, rows: 1, color: '#fff' },
-    { text: 'Wednesday', cols: 2, rows: 1, color: '#fff' },
-    { text: 'Thursday', cols: 2, rows: 1, color: '#fff' },
-    { text: 'Friday', cols: 2, rows: 1, color: '#fff' },
-
-    { text: '8AM', cols: 1, rows: 4, color: '#ddd' },
-
-    { text: "main", cols: 10, rows: 52, color: '' },
-
-    { text: '9AM', cols: 1, rows: 4, color: '#ddd' },
-    { text: '10AM', cols: 1, rows: 4, color: '#ddd' },
-    { text: '11AM', cols: 1, rows: 4, color: '#ddd' },
-    { text: '12PM', cols: 1, rows: 4, color: '#ddd' },
-    { text: '1PM', cols: 1, rows: 4, color: '#ddd' },
-    { text: '2PM', cols: 1, rows: 4, color: '#ddd' },
-    { text: '3PM', cols: 1, rows: 4, color: '#ddd' },
-    { text: '4PM', cols: 1, rows: 4, color: '#ddd' },
-    { text: '5PM', cols: 1, rows: 4, color: '#ddd' },
-    { text: '6PM', cols: 1, rows: 4, color: '#ddd' },
-    { text: '7PM', cols: 1, rows: 4, color: '#ddd' },
-    { text: '8PM', cols: 1, rows: 4, color: '#ddd' },
-  ];
-
+  // mainGrid: number[] = [];
 
   constructor() {
-    this.mainGrid = Array.from(Array(13 * 4), (x, i) => i);
+    // this.mainGrid = Array.from(Array(13 * 4), (x, i) => i);
   }
 
-  ngOnInit() {
+  ngAfterViewInit() {
+    console.log("COLS")
+    console.log(this.weekdayCols);
 
   }
 
   displayCourse() {
     let course = { days: 'M', start: '8:30', end: "9:45" };
+    this.weekdayCols[0]
     // this.mainGrid[60] = { text: '60', cols: 1, rows: 4, color: 'pink' }
   }
+
+
+
+
 
 }
--- src/app/calendar/day-column/day-column.component.ts
+++ src/app/calendar/day-column/day-column.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DayColumnComponent implements OnInit {
 
+  hours: String[] = ["8AM", "9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM", "6PM", "7PM", "8PM"];
+
   constructor() { }
 
   ngOnInit() {
