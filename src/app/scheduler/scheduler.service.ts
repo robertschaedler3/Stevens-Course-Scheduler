1d602285cc3fd36e68f5513b5adac605f6f74e7b
Robert Schaedler III <robertschaedler3@gmail.com>
2019-10-29 00:00:54 -0400

    added section selection for course

--- src/app/scheduler/scheduler.service.ts
+++ src/app/scheduler/scheduler.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SchedulerService {
 
+  private selectedData = [];
+  private coursesData = {}
+
+  private selectedList = new BehaviorSubject(['']);
+  selected = this.selectedList.asObservable();
+
+  private coursesDict = new BehaviorSubject({});
+  courses = this.coursesDict.asObservable();
+
+
   constructor() { }
+
+  addSelected(key) {
+    this.selectedData.push(key);
+    this.selectedList.next(this.selectedData);
+    console.log(this.coursesData[key]);
+  }
+
+  setCourses(courses) {
+    this.coursesData = courses;
+    this.coursesDict.next(this.coursesData);
+  }
+
 }
--- src/app/selected-options/selected-options.component.ts
+++ src/app/selected-options/selected-options.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SchedulerService } from '../scheduler/scheduler.service';
 
 @Component({
   selector: 'app-selected-options',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SelectedOptionsComponent implements OnInit {
 
-  constructor() { }
+  selected: string[];
+  courses = {};
+
+  constructor(public scheduler: SchedulerService) { }
 
   ngOnInit() {
+    this.scheduler.selected.subscribe(data => this.selected = data);
+    this.scheduler.courses.subscribe(data => this.courses = data);
+  }
+
+  getSection(section: string): string {
+    let start = 0;
+    for (let i = section.length - 1; i > 0; i--) {
+      if (!isNaN(parseInt(section.charAt(i)))) {
+        break;
+      }
+      start = i;
+    }
+    return section.substring(start);
   }
 
 }
