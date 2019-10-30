063ee08231df501e9992f058a6b46c7563a32521
Robert Schaedler III <robertschaedler3@gmail.com>
2019-10-30 01:44:13 -0400

    create study plan component

---
+++ src/app/studyplan/studyplan.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { StudyplanComponent } from './studyplan.component';
+
+describe('StudyplanComponent', () => {
+  let component: StudyplanComponent;
+  let fixture: ComponentFixture<StudyplanComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ StudyplanComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(StudyplanComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/studyplan/studyplan.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-studyplan',
+  templateUrl: './studyplan.component.html',
+  styleUrls: ['./studyplan.component.scss']
+})
+export class StudyplanComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
