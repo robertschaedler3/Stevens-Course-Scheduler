1a98d6e5aa05c57e5cb982ddd9195c25025d8c2b
Robert Schaedler III <robertschaedler3@gmail.com>
2019-10-30 01:43:55 -0400

    create saved schedule component

---
+++ src/app/saved-schedules/saved-schedules.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-saved-schedules',
+  templateUrl: './saved-schedules.component.html',
+  styleUrls: ['./saved-schedules.component.scss']
+})
+export class SavedSchedulesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
