4a91133d6587130c2f3fc9a2f116c607296661b6
Robert Schaedler III <robertschaedler3@gmail.com>
2019-10-30 00:43:11 -0400

    add sections selection

--- src/app/selected-options/selected-options.component.ts
+++ src/app/selected-options/selected-options.component.ts
@@ -1,21 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { SchedulerService } from '../scheduler/scheduler.service';
+import { trigger, state, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-selected-options',
   templateUrl: './selected-options.component.html',
-  styleUrls: ['./selected-options.component.scss']
+  styleUrls: ['./selected-options.component.scss'],
+  animations: [
+    trigger('selectorState', [
+      state('theme', style({
+        background: '#9c1c30',
+        color: '#fff'
+      })),
+      state('white', style({
+        background: '#fff',
+        color: '#eee'
+      })),
+      transition('theme => white', animate('1000ms ease-out')),
+      transition('white => theme', animate('1000ms ease-in'))
+    ])
+  ]
 })
+
 export class SelectedOptionsComponent implements OnInit {
 
   selected: string[];
   courses = {};
+  sections = {};
 
   constructor(public scheduler: SchedulerService) { }
 
   ngOnInit() {
     this.scheduler.selected.subscribe(data => this.selected = data);
     this.scheduler.courses.subscribe(data => this.courses = data);
+    this.scheduler.sections.subscribe(data => this.sections = data);
   }
 
   getSection(section: string): string {
@@ -27,6 +45,12 @@ export class SelectedOptionsComponent implements OnInit {
       start = i;
     }
     return section.substring(start);
+  }
+
+  sectionToggled(event) {
+    const section = event.value;
+    const callNumber = section.callNumber;
+    this.scheduler.addSection(section, callNumber);
   }
 
 }
