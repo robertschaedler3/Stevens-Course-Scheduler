018ef1d4473298134474db0f40a02f50f31cc91d
Robert Schaedler III <robertschaedler3@gmail.com>
2019-10-30 00:42:06 -0400

    added section data

--- src/app/scheduler/scheduler.service.ts
+++ src/app/scheduler/scheduler.service.ts
@@ -6,15 +6,18 @@ import { BehaviorSubject } from 'rxjs';
 })
 export class SchedulerService {
 
+  private sectionData = {};
   private selectedData = [];
   private coursesData = {}
+
+  private sectionDict = new BehaviorSubject({});
+  sections = this.sectionDict.asObservable();
 
   private selectedList = new BehaviorSubject(['']);
   selected = this.selectedList.asObservable();
 
   private coursesDict = new BehaviorSubject({});
   courses = this.coursesDict.asObservable();
-
 
   constructor() { }
 
@@ -27,6 +30,15 @@ export class SchedulerService {
   setCourses(courses) {
     this.coursesData = courses;
     this.coursesDict.next(this.coursesData);
+  }
+
+  addSection(section, callNumber) {
+    if (this.sectionData[callNumber]) {
+      delete this.sectionData[callNumber];
+    } else {
+      this.sectionData[callNumber] = section;
+    }
+    this.sectionDict.next(this.sectionData)
   }
 
 }
