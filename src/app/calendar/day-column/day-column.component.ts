3d8e7054d0901d19a0c7672b45e1433bbb8c4916
Robert Schaedler III <robertschaedler3@gmail.com>
2019-10-30 00:38:21 -0400

    unified naming of data fields

--- src/app/calendar/day-column/day-column.component.ts
+++ src/app/calendar/day-column/day-column.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
 
-
 @Component({
   selector: 'app-day-column',
   templateUrl: './day-column.component.html',
@@ -10,25 +9,27 @@ export class DayColumnComponent implements OnInit {
 
   sections = {};
   ids = [];
-
   hours: String[] = ["8AM", "9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM", "6PM", "7PM", "8PM"];
 
   @Input() day: string = '';
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() { }
 
-  }
-
-  public addCourse(id: string, startTime: string, endTime: string) {
+  public addCourse(name, code, id, start, end) {
     this.ids.push(id);
     this.sections[id] = {
-      'name': 'AAI 800A: Special Prob in AAI(Masters)',
-      'id': id,
-      'start': startTime,
-      'end': endTime
+      'name': name,
+      'code': code,
+      'start': start,
+      'end': end
     }
+  }
+
+  reset() {
+    this.sections = {}
+    this.ids = [];
   }
 
 }
