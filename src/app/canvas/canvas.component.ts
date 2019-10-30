fe3f3b4fd35be2916f8d30dd12b828e9f6555d51
Robert Schaedler III <robertschaedler3@gmail.com>
2019-10-30 01:50:33 -0400

    add signin

--- src/app/canvas/canvas.component.ts
+++ src/app/canvas/canvas.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+// import { AuthService } from '../services/auth.service';
+
 @Component({
   selector: 'app-canvas',
   templateUrl: './canvas.component.html',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CanvasComponent implements OnInit {
 
-  constructor() { }
+  signinForm: FormGroup;
+
+  constructor(
+    // public auth: AuthService,
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.signinForm = this.fb.group({
+      email: ['', [
+        Validators.required,
+        Validators.email,
+        Validators.pattern('^[a-z0-9](\.?[a-z0-9]){5,}@stevens\.edu$')
+      ]],
+      password: ['', [
+        Validators.required,
+        Validators.minLength(8)
+      ]]
+    });
+  }
+
+  async onSubmit() {
+    const formValue = this.signinForm.value;
+    var email = formValue.email;
+    var pass = formValue.password;
+    // this.auth.signIn(email, pass);
   }
 
 }
