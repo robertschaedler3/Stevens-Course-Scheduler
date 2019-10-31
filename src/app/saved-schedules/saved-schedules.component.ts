99853c1ff768d7ea7359984d3660c10ecf39506f
Robert Schaedler III <robertschaedler3@gmail.com>
2019-10-31 00:22:05 -0400

    added save schedule to local storage

--- src/app/saved-schedules/saved-schedules.component.ts
+++ src/app/saved-schedules/saved-schedules.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LocalStorageService } from 'app/storage/localstorage.service';
 
 @Component({
   selector: 'app-saved-schedules',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SavedSchedulesComponent implements OnInit {
 
-  constructor() { }
+  names: Array<string>;
+  schedules = {};
+
+  constructor(private storage: LocalStorageService) { }
 
   ngOnInit() {
+    this.names = this.storage.getNames();
+    this.names.forEach(name => {
+      this.schedules[name] = this.storage.getSchedule(name);
+      console.log(this.storage.getSchedule(name))
+    })
   }
 
 }
--- src/app/storage/localstorage.service.ts
+++ src/app/storage/localstorage.service.ts
@@ -1,9 +1,41 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
+import { LOCAL_STORAGE, StorageService } from 'ngx-webstorage-service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class StorageService {
+export class LocalStorageService {
+  anotherTodolist = [];
 
-  constructor() { }
+  constructor(@Inject(LOCAL_STORAGE) private storage: StorageService) { }
+
+  public saveSchedule(name, data) {
+    let scheduleNames = this.storage.get('schedules') || [];
+    if (scheduleNames.indexOf(name) > 0) {
+      if (!confirm("Overwrite a schedule with the same name: " + name)) {
+        return;
+      }
+    }
+    scheduleNames.push(name);
+    this.storage.set('schedules', scheduleNames);
+    this.storage.set(name, data);
+  }
+
+  public removeSchedule(name) {
+    let scheduleNames: Array<string> = this.storage.get('schedules') || [];
+    scheduleNames.slice(scheduleNames.indexOf(name), 1);
+    this.storage.remove(name);
+  }
+
+  public getNames() {
+    return this.storage.get('schedules') || [];
+  }
+
+  public getSchedule(name) {
+    return this.storage.get(name) || [];
+  }
+
+  clear() {
+    this.storage.clear();
+  }
 }
