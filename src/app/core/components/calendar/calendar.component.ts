5536707ea107b57d965882b04c5ba6a16e977e91
Robert Schaedler III <robertschaedler3@gmail.com>
2020-08-02 18:53:07 -0400

    create base calendar layout

--- src/app/core/components/calendar/calendar.component.ts
+++ src/app/core/components/calendar/calendar.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+export const WEEKDAYS: string[] = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+
+export const TIMES: number[] = [8, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8];
+
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.scss']
 })
 export class CalendarComponent implements OnInit {
+
+  weekDays: string[] = WEEKDAYS;
+  times: number[] = TIMES;
 
   constructor() { }
 
