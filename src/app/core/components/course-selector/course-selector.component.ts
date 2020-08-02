cbafcbf4ad0ae5a34a8a029d1ba927149b06caa3
Robert Schaedler III <robertschaedler3@gmail.com>
2020-08-02 17:20:23 -0400

    create course-seletor component

---
+++ src/app/core/components/course-selector/course-selector.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-course-selector',
+  templateUrl: './course-selector.component.html',
+  styleUrls: ['./course-selector.component.scss']
+})
+export class CourseSelectorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CalendarComponent } from './components/calendar/calendar.component';
+import { TermSelectorComponent } from './components/term-selector/term-selector.component';
+import { CourseSelectorComponent } from './components/course-selector/course-selector.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [CalendarComponent, TermSelectorComponent, CourseSelectorComponent],
   imports: [
     CommonModule
   ]
