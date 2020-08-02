79960a9f3b1bd4b45483a208a463e7d81ecefe80
Robert Schaedler III <robertschaedler3@gmail.com>
2020-08-02 17:24:22 -0400

    create scheduler layout component

--- src/app/layouts/layouts.module.ts
+++ src/app/layouts/layouts.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SchedulerComponent } from './scheduler/scheduler.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [SchedulerComponent],
   imports: [
     CommonModule
   ]
