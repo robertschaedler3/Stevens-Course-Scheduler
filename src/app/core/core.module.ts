dd1e27818e24d83fbf4ba90ce4e0978eae7f3a09
Robert Schaedler III <robertschaedler3@gmail.com>
2020-08-02 17:22:08 -0400

    update core module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CalendarComponent } from './components/calendar/calendar.component';
-import { TermSelectorComponent } from './components/term-selector/term-selector.component';
-import { CourseSelectorComponent } from './components/course-selector/course-selector.component';
 
+import { CalendarComponent } from './components/calendar/calendar.component';
+import { CourseSelectorComponent } from './components/course-selector/course-selector.component';
+import { TermSelectorComponent } from './components/term-selector/term-selector.component';
 
 
 @NgModule({
-  declarations: [CalendarComponent, TermSelectorComponent, CourseSelectorComponent],
+  declarations: [
+    CalendarComponent,
+    CourseSelectorComponent,
+    TermSelectorComponent
+  ],
   imports: [
     CommonModule
   ]
