4dd5d82115d6d3cf0a7e4a84bcf69ad45ec4e468
Robert Schaedler III <robertschaedler3@gmail.com>
2020-08-02 17:21:34 -0400

    create term-selector component

---
+++ src/app/core/components/term-selector/term-selector.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-term-selector',
+  templateUrl: './term-selector.component.html',
+  styleUrls: ['./term-selector.component.scss']
+})
+export class TermSelectorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
