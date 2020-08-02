e5a0244c290bd5ba7370b66f65d91458983226f9
Robert Schaedler III <robertschaedler3@gmail.com>
2020-08-02 17:38:54 -0400

    create base app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+// Route Components
+import { SchedulerComponent } from './layouts/scheduler/scheduler.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: SchedulerComponent },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
