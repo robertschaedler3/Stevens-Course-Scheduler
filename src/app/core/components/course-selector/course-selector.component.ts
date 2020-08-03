539b3452139438a9db4aeb2ebb16431e56b1d9e8
Robert Schaedler III <robertschaedler3@gmail.com>
2020-08-03 16:06:59 -0400

    add course search filter

--- src/app/core/components/course-selector/course-selector.component.ts
+++ src/app/core/components/course-selector/course-selector.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { startWith, map } from 'rxjs/operators';
+
+import { ScheduleSharedViewStateService } from 'src/app/services/schedule-shared-view-state.service';
+import { Course } from 'src/app/types/scheduler';
 
 @Component({
   selector: 'app-course-selector',
@@ -7,9 +13,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CourseSelectorComponent implements OnInit {
 
-  constructor() { }
+  ctrl = new FormControl();
+  courses: Course[] = [];
+  filteredCourses: Observable<Course[]>;
 
-  ngOnInit(): void {
+  constructor(public scheduleData: ScheduleSharedViewStateService) {
+    this.scheduleData.courses$.subscribe(courses => this.courses = courses);
+  }
+
+  ngOnInit() {
+    this.filteredCourses = this.ctrl.valueChanges
+      .pipe(
+        startWith(''),
+        map(value => this._filter(value))
+      );
+  }
+
+  private _filter(value: string): Course[] {
+    const filterValue = value.toLowerCase();
+
+    return this.courses.filter(course => course.section.toLowerCase().includes(filterValue)).slice(0, 10);
   }
 
 }
